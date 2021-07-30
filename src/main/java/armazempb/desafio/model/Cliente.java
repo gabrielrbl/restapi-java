c7152ed7dea1e221419d834db2eb3bff5599bc34
Gabriel Lobo <gabrielrblgbi@gmail.com>
2021-07-30 15:50:17 -0300

    Refatoração

--- src/main/java/armazempb/desafio/model/Cliente.java
+++ src/main/java/armazempb/desafio/model/Cliente.java
@@ -10,39 +10,57 @@ import javax.persistence.Id;
 
 @Entity
 public class Cliente {
-	private @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id") Long idCliente;
+	private @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id") Integer idCliente;
 	private @Column(name = "nome") String nome;
 	private @Column(name = "cpf") String cpf;
 	private @Column(name = "endereco") String endereco;
 	private @Column(name = "data_nascimento") Date dataNascimento;
 	
-	public Long getIdCliente() {
-		return idCliente;
+	public Cliente () {
+		super();
 	}
-	public void setIdCliente(Long idCliente) {
+
+	public Cliente(Integer idCliente) {
+		super();
 		this.idCliente = idCliente;
 	}
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
--- src/main/java/armazempb/desafio/model/Pedido.java
+++ src/main/java/armazempb/desafio/model/Pedido.java
@@ -2,7 +2,6 @@ package armazempb.desafio.model;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,32 +12,39 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Pedido {
-	private @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id") Long idPedido;
-	private @OneToOne(cascade = CascadeType.ALL) @JoinColumn(name = "id_cliente", referencedColumnName = "id") Cliente cliente;
+	private @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id") Integer idPedido;
+	private @OneToOne @JoinColumn(name = "id_cliente", referencedColumnName = "id") Cliente cliente;
 	private @Column(name = "valor_total") Double valorTotal;
 	private @Column(name = "data_pedido") Date dataPedido;
-	
-	public Long getIdPedido() {
+
+	public Integer getIdPedido() {
 		return idPedido;
 	}
-	public void setIdPedido(Long idPedido) {
+
+	public void setIdPedido(Integer idPedido) {
 		this.idPedido = idPedido;
 	}
+
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	public Double getValorTotal() {
 		return valorTotal;
 	}
+
 	public void setValorTotal(Double valorTotal) {
 		this.valorTotal = valorTotal;
 	}
+
 	public Date getDataPedido() {
 		return dataPedido;
 	}
+
 	public void setDataPedido(Date dataPedido) {
 		this.dataPedido = dataPedido;
 	}
